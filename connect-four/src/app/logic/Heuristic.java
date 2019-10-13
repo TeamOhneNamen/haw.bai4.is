51a2f432d9f8ad7898c29216658749e3de356967
Thorben <>
2019-10-13 15:31:18 +0200

    1: heuristik als fsm verbessert + umsetzung im Code fehlerhaft

--- connect-four/src/app/logic/Heuristic.java
+++ connect-four/src/app/logic/Heuristic.java
@@ -10,6 +10,9 @@ public class Heuristic {
     }
 
     protected static double determineHorizontalScore(String playerColor) {
+        int emptyLeftNeighbors = 0;
+        int emptyRightNeighbors = 0;
+        int inARow = 0;
         double score = 0;
         for (int i = 0; i < Board.rowLength(); i++) {
             int j = 0;
@@ -19,35 +22,74 @@ public class Heuristic {
                 j += 1;
                 // 1 in a row
                 if (playerColor.equals(Board.get(i, column))) {
-                    score++;
+                    inARow++;
                     // space to the left
                     if(Board.NO_COLOR.equals(Board.get(i, column - 1))){
-                        score += 0.5;
+                        emptyLeftNeighbors++;
+                        if(Board.NO_COLOR.equals(Board.get(i, column - 2))){
+                            emptyLeftNeighbors++;
+                            if(Board.NO_COLOR.equals(Board.get(i, column - 3))){
+                                emptyLeftNeighbors++;
+                            }
+                        }
                     }
                     j += 2;
                     // 2 in a row
                     if (playerColor.equals(Board.get(i, column + 1))) {
-                        score++;
+                        inARow++;
                         j += 3;
                         // 3 in a row
                         if (playerColor.equals(Board.get(i, column + 2))) {
-                            score++;
+                            inARow++;
                             // 3 in a row with space to the right
                             if (Board.NO_COLOR.equals(Board.get(i, column + 3))) {
                                 score += 0.5;
                             }
                             System.out.println("3 in a row with space to the right");
                             j += 4;
-                            // 2 in a row with space to the right
+                            // 2 in a row with 1 space to the right
                         } else if (Board.NO_COLOR.equals(Board.get(i, column+3))) {
-                            score += 0.5;
+                            emptyRightNeighbors++;
+                            j += 4;
+                            // 2 in a row with 2 space to the right
+                            if (Board.NO_COLOR.equals(Board.get(i, column + 4))) {
+                                emptyRightNeighbors++;
+                                j += 5;
+                            }
                         }
-                        // 1 in a row with space to the right
+                        // 1 in a row with 1 space to the right
                     } else if (Board.NO_COLOR.equals(Board.get(i, column+2))) {
-                        score += 0.5;
+                        emptyRightNeighbors++;
+                        j += 3;
+                        // 1 in a row with 2 space to the right
+                        if (Board.NO_COLOR.equals(Board.get(i, column + 3))) {
+                            emptyRightNeighbors++;
+                            j += 4;
+                            // 1 in a row with 2 space to the right
+                            if (Board.NO_COLOR.equals(Board.get(i, column + 4))) {
+                                emptyRightNeighbors++;
+                                j += 5;
+
+                            }
+                        }
+
                     }
                 }
 
+            }
+        }
+        return evaluate(emptyLeftNeighbors, emptyRightNeighbors, inARow);
+    }
+
+    private static double evaluate(int emptyLeftNeighbors, int emptyRightNeighbors, int inARow) {
+        double score = 0.0;
+        if(emptyLeftNeighbors+emptyRightNeighbors+inARow >3 ){
+            score = inARow;
+            if(emptyRightNeighbors > 0){
+                score += 0.5;
+            }
+            if(emptyRightNeighbors > 0){
+                score += 0.5;
             }
         }
         return score;
